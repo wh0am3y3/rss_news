35722fedddfd6d0ac79d30b43ca5156fee4e5870
wh0am3y3 <43875109+wh0am3y3@users.noreply.github.com>
2020-09-11 17:27:15 +0300

    added feedparser snippet to code

--- front_page.py
+++ front_page.py
@@ -1,4 +1,5 @@
 from datetime import date
+import feedparser
 from flask import Flask, render_template
 import os
 
@@ -6,13 +7,17 @@ import os
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
+    Feed = feedparser.parse('http://www.reddit.com/r/python/.rss')
+    for pointer in Feed.entries:
+        feed_title = pointer.title
+        feed_link = pointer.link
     user = os.getlogin()
     today = date.today().strftime('%B %d %Y')
-    return render_template('index.html', user=user, today=today)
+    return render_template('index.html', user=user, today=today, feed_title=feed_title, feed_link=feed_link)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
+
