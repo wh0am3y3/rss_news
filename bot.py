70e3028cd7c54b63c6955724c60509c956c9ab25
wh0am3y3 <43875109+wh0am3y3@users.noreply.github.com>
2020-09-11 23:38:24 +0300

    fixed bot script

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver import Firefox
+from time import sleep
 
 
-firefox_capabilities = DesiredCapabilities.FIREFOX
-firefox_capabilities['marionette'] = False
-# firefox_capabilities['binary'] = '/usr/bin/firefox'
-binary = FirefoxBinary("/usr/lib/firefox/firefox")
-fp = webdriver.FirefoxProfile()
-driver = webdriver.Firefox(
-        capabilities=firefox_capabilities,
-        firefox_binary=binary,
-        firefox_profile=fp,
-        executable_path="./geckodriver"
-        )
-driver.get("http://www.google.com")
-# driver.close()
-# driver.quit()
+def browser():
+    driver = Firefox(executable_path="./geckodriver")
+    driver.get("http://127.0.0.1:5000")
+    sleep(10)
+    driver.close()
+    quit()
+
+
+if __name__ == '__main__':
+    try:
+        browser()
+    except Exception as e:
+        print(e)
+        quit()
